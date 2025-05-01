55ae48342827d336a10bc18ddaba6a388b084663
João Demutti <joaofacindemutti@gmail.com>
2025-05-01 14:17:47 -0300

    backend deploy

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '../../enviroments/enviroment'; // Import environment for API URL
+import { environment } from '../../environments/environment'; // Import environment for API URL
 import { tap,switchMap } from 'rxjs/operators';
 
 @Injectable({
